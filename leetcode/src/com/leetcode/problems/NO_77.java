67e87acd186a7e7280b0ef023c781e896c089b44
mzxl1987 <448632824@qq.com>
2018-12-26 15:47:25 +0800

    1.77. Combinations

---
+++ leetcode/src/com/leetcode/problems/NO_77.java
@@ -0,0 +1,30 @@
+package com.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NO_77 {
+
+	List<List<Integer>> list = new ArrayList<>();
+	List<Integer> tmp = new ArrayList<>();
+	
+	public List<List<Integer>> combine(int n, int k) {
+		append(tmp,n,k);
+        return list;
+    }
+	
+	public void append(List<Integer> result,int n, int k){
+		
+		if(k == 0){
+			list.add(new ArrayList<>(result));
+			return ;
+		}
+		
+		for(int i = n; i > 0; i--){ 
+			tmp.add(i);
+			append(tmp,i - 1,k - 1);
+			tmp.remove(tmp.size() - 1);
+		}
+	}
+	
+}
