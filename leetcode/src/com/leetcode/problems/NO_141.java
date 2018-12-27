8f8f987994c3d0840e380507b48709bd18d7962f
mzxl1987 <448632824@qq.com>
2018-12-27 15:55:05 +0800

    1.141  Linked List Cycle

--- leetcode/src/com/leetcode/problems/NO_141.java
+++ leetcode/src/com/leetcode/problems/NO_141.java
@@ -14,9 +14,31 @@ public class NO_141 {
 			if(go == null || go.next == null) return false;
 			follow = follow.next;
 			go = go.next.next;
+			
+			System.out.println(String.format("fast : %d, slow : %d", go.val,follow.val));
 		}
 		
 		return true;
     }
+	
+	public static void main(String[] args) {
+		
+		NO_141 t = new NO_141();
+		ListNode dom = new ListNode(0);
+		ListNode tmp = dom;
+		
+		for(int i = 1; i <= 10; i++){
+			tmp.next = new ListNode(i);
+			tmp = tmp.next;
+		}
+		
+		ListNode loopPoint = dom.next.next.next;
+		System.out.println(loopPoint.val);
+		
+		tmp.next = loopPoint;
+		
+		System.out.println(t.hasCycle(dom.next));
+		
+	}
 	
 }
